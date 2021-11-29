4c9b809fbbba70d027e4cab2f1e102a64fb71a76
Vladimir <kvv23@tut.by>
2021-11-29 23:58:34 +0300

    lesson_9. Формирование контекстного процессора для корзины.

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 
-from basketapp.models import Basket
+# from basketapp.models import Basket
 
 from .models import Contact, Product, ProductCategory
 
@@ -17,11 +17,11 @@ def main(request):
     return render(request, "mainapp/index.html", content)
 
 
-def get_basket(user):
-    if user.is_authenticated:
-        return Basket.objects.filter(user=user)
-    else:
-        return []
+# def get_basket(user):
+#     if user.is_authenticated:
+#         return Basket.objects.filter(user=user)
+#     else:
+#         return []
 
 
 def get_hot_product():
@@ -37,7 +37,7 @@ def get_same_products(hot_product):
 def products(request, pk=None, page=1):
     title = "продукты"
     links_menu = ProductCategory.objects.filter(is_active=True)
-    basket = get_basket(request.user)
+    # basket = get_basket(request.user)
 
     if pk is not None:
         if pk == '0':
@@ -63,7 +63,7 @@ def products(request, pk=None, page=1):
             "category": category,
             "products": products_paginator,
             "media_url": settings.MEDIA_URL,
-            "basket": basket,
+            # "basket": basket,
         }
         return render(request, "mainapp/products_list.html", content)
     hot_product = get_hot_product()
@@ -73,8 +73,8 @@ def products(request, pk=None, page=1):
         "links_menu": links_menu,
         "same_products": same_products,
         "media_url": settings.MEDIA_URL,
-        "basket": basket,
         "hot_product": hot_product,
+        # "basket": basket,
     }
     return render(request, "mainapp/products.html", content)
 
